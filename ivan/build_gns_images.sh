52ecabfc511a18f12724f35f93193aa34ad39794
Sapir <sapirani@post.bgu.ac.il>
2025-05-22 13:49:24 +0300

    add bash init

--- ivan/build_gns_images.sh
+++ ivan/build_gns_images.sh
@@ -1,28 +1,56 @@
+#!/bin/sh
+
 sudo ./base/build_base.sh
+
+wait
 
 sudo ./measurements_base/build_measurements.sh
 
+wait
+
 sudo ./environment_setup/build_hadoop_env.sh
+
+wait
 
 cd ./resourcemanager
 
+wait
+
 sudo docker build -t resourcemanager .
 
+wait
+
 cd ..
+
+wait
 
 cd ./namenode
 
+wait
+
 sudo docker build -t namenode .
 
+wait
+
 cd ..
+
+wait
 
 cd ./datanode
 
+wait
+
 sudo docker build -t datanode .
+
+wait
 
 cd ..
 
+wait
+
 cd ./historyserver
+
+wait
 
 sudo docker build -t historyserver .
 
