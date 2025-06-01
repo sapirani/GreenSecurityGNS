e18c65ea1a988e499dcd6c97662370e59750a209
Sapir <sapirani@post.bgu.ac.il>
2025-06-01 23:30:41 +0300

    update memory docker stats

--- ivan/nodes_configuration_code/docker_stats/memory_docker_stats.py
+++ ivan/nodes_configuration_code/docker_stats/memory_docker_stats.py
@@ -1,2 +1,15 @@
-def calculate_memory_percent(stat) -> float:
-    return 0.0 #TODO: change
+class DockerMemoryConsts:
+    MEMORY_STATS = "memory_stats"
+    USAGE = "usage"
+    MAX_USAGE = "max_usage"
+    LIMIT = "limit"
+
+def get_memory_usage(stat) -> tuple[float, float]:
+    """
+    Returns a dictionary with current memory usage, max usage, and memory limit in bytes.
+    """
+    memory_stats = stat.get(DockerMemoryConsts.MEMORY_STATS, {})
+    memory_usage = memory_stats.get(DockerMemoryConsts.USAGE, 0)
+    memory_limit = memory_stats.get(DockerMemoryConsts.LIMIT, 1)
+    memory_percent = memory_usage / memory_limit * 100
+    return memory_usage, memory_percent
--- ivan/nodes_configuration_code/docker_stats/print_docker_stats.py
+++ ivan/nodes_configuration_code/docker_stats/print_docker_stats.py
@@ -2,6 +2,7 @@ import docker
 import time
 
 from ivan.nodes_configuration_code.docker_stats.cpu_docker_stats import calculate_cpu_percent
+from ivan.nodes_configuration_code.docker_stats.memory_docker_stats import get_memory_usage
 
 client = docker.from_env()
 
@@ -9,6 +10,6 @@ while True:
     for container in client.containers.list():
         stats = container.stats(stream=False)
         cpu = calculate_cpu_percent(stats)
-        memory = calculate_memory_usage(stats)
-        print(f"{container.name}: {cpu:.2f}% CPU, {memory:.2f}%")
+        memory_usage, memory_percent = get_memory_usage(stats)
+        print(f"{container.name}: {cpu:.2f}% CPU, {memory_percent:.2f}% RAM, {memory_usage:.2f}GB RAM")
     time.sleep(2)
