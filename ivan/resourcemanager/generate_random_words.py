8cb4c89f21cfafeae42cbc3b55f9c1642c7bbd9f
sagi-brudni <63012265+sagi-brudni@users.noreply.github.com>
2026-01-23 09:24:15 +0200

    Organize distributed code (#29)
    * Organize distributed code
    
    * Improve word generation
    
    * Modify permissions
    
    * Activate API inside the resource manager
    
    * remove files dockerfile
    
    * Refactor

---
+++ ivan/resourcemanager/generate_random_words.py
@@ -0,0 +1,55 @@
+import subprocess
+import argparse
+
+KB = 1024
+MB = 1024 * KB
+GB = 1024 * MB
+
+DEFAULT_FILE_SIZE_GB = 1.0
+DEFAULT_LEN_OF_WORD = 5
+DEFAULT_FILE_NAME = "input.txt"
+
+
+def write_to_hdfs(words_count: int, len_word: int, f_name: str):
+    subprocess.run(
+        f"tr -dc 'a-z' < /dev/urandom | fold -w {len_word} | head -n {words_count} | hadoop fs -put - {f_name}",
+        shell=True,
+        check=True
+    )
+
+
+def write_to_file(words_count: int, len_word: int, f_name: str):
+    with open(f_name, "wb") as f:
+        subprocess.run(
+            f"tr -dc 'a-z' < /dev/urandom | fold -w {len_word} | head -n {words_count}",
+            stdout=f,
+            shell=True,
+            check=True
+        )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Generate random lowercase words")
+    parser.add_argument("-l", "--word_length", type=int, default=DEFAULT_LEN_OF_WORD,
+                        help="Length of each word (default: 5)")
+    parser.add_argument("-s", "--size_gb", type=float, default=DEFAULT_FILE_SIZE_GB,
+                        help="Total output size in GB")
+    parser.add_argument("-o", "--output_path", type=str, default=DEFAULT_FILE_NAME,
+                        help="Output file")
+    parser.add_argument(
+        "-L", "--to_local",
+        action="store_true",
+        default=False,
+        help="Stream output directly into HDFS or locally (default: HDFS)"
+    )
+
+    args = parser.parse_args()
+    size_bytes = int(args.size_gb * GB)
+    num_of_words = int(size_bytes / (args.word_length + 1))   # one character is required for a new line
+
+    if args.to_local:
+        write_to_file(num_of_words, args.word_length, args.output_path)
+    else:
+        write_to_hdfs(num_of_words, args.word_length, args.output_path)
+    print(f"{num_of_words} random words written to {args.output_path if args.to_local else 'HDFS: ' + args.output_path}")
+
--- ivan/resourcemanager/run.sh
+++ ivan/resourcemanager/run.sh
@@ -5,6 +5,7 @@
 sleep 40
 
 PYTHONPATH=/green_security_measurements/Scanner /green_security_measurements/green_security_venv/bin/python -m scanner_trigger.trigger_receiver --python_path=/green_security_measurements/green_security_venv/bin/python --scanner_path=/green_security_measurements/Scanner/scanner.py -n=19  &
+PYTHONPATH=/green_security_measurements/Scanner /green_security_measurements/green_security_venv/bin/python -m hadoop_optimizer.training_server.api.main  &
 
 $HADOOP_HOME/bin/yarn --config $HADOOP_CONF_DIR resourcemanager > resourcemanager_log.txt &
 bash
