6da615f626fdf6748b10081fbaf8c75d3cafa71b
sagi-brudni <brudnis@post.bgu.ac.il>
2026-05-30 01:21:37 +0300

    generate random words in advance

--- ivan/resourcemanager/run.sh
+++ ivan/resourcemanager/run.sh
@@ -2,6 +2,10 @@
 
 #TODO: add health check and use depends in the docker compose
 
+python /home/generate_random_words.py -s 0.3 -o /input/0.3_gb &
+python /home/generate_random_words.py -s 0.7 -o /input/0.7_gb &
+python /home/generate_random_words.py -s 1.0 -o /input/1.0_gb &
+
 sleep 40
 
 PYTHONPATH=/green_security_measurements/Scanner /green_security_measurements/green_security_venv/bin/python -m scanner_trigger.trigger_receiver --python_path=/green_security_measurements/green_security_venv/bin/python --scanner_path=/green_security_measurements/Scanner/scanner.py -n=19  &
