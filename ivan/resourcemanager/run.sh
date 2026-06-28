d35708d873c8d711da0bec4b840688fd35bf4d36
sagi-brudni <brudnis@post.bgu.ac.il>
2026-06-28 21:11:37 +0300

    Bugfix

--- ivan/resourcemanager/run.sh
+++ ivan/resourcemanager/run.sh
@@ -2,14 +2,16 @@
 
 #TODO: add health check and use depends in the docker compose
 
-python /home/generate_random_words.py -s 0.3 -o /input/0.3_gb &
-python /home/generate_random_words.py -s 0.7 -o /input/0.7_gb &
-python /home/generate_random_words.py -s 1.0 -o /input/1.0_gb &
-
 sleep 40
 
 PYTHONPATH=/green_security_measurements/Scanner /green_security_measurements/green_security_venv/bin/python -m scanner_trigger.trigger_receiver --python_path=/green_security_measurements/green_security_venv/bin/python --scanner_path=/green_security_measurements/Scanner/scanner.py -n=19  &
 PYTHONPATH=/green_security_measurements/Scanner /green_security_measurements/green_security_venv/bin/python -m hadoop_optimizer.job_runner.server.api.main  &
+
+python /home/generate_random_words.py -s 0.3 -o /input/0.3_gb &
+python /home/generate_random_words.py -s 0.7 -o /input/0.7_gb &
+python /home/generate_random_words.py -s 1.0 -o /input/1.0_gb &
+
+wait
 
 $HADOOP_HOME/bin/yarn --config $HADOOP_CONF_DIR resourcemanager > resourcemanager_log.txt &
 bash
