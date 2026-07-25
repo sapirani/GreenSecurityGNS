b1e7c91d253e5d425d76a5f22cf1ac9e66624f8c
sagi-brudni <brudnis@post.bgu.ac.il>
2026-07-25 19:19:13 +0300

    Deleting all nodes before creating new topology

--- ivan/build_full_topology.py
+++ ivan/build_full_topology.py
@@ -1,5 +1,5 @@
+import argparse
 import asyncio
-import json
 import aiohttp
 import subprocess
 import yaml
@@ -10,7 +10,7 @@ with open("config.yaml") as f:
     PASSWORD = config["password"]
 
 GNS3_URL = "http://192.168.140.110:3080/v2"
-PROJECT_ID = "7ea71294-5410-4032-90aa-a2e6acdfb2ae"
+PROJECT_ID = "8b1df7a1-d0ea-4bd9-a5d7-f0c6bb61b40b"
 
 hadoop_nodes_replicas = {
     "resourcemanager": 1,
@@ -34,7 +34,7 @@ total_hadoop_devices = sum(hadoop_nodes_replicas.values())
 
 
 async def create_single_node(session, auth, template_name, template_id, device_num):
-    """Worker task to create an individual node concurrently."""
+    """Worker task to create an individual node."""
     global switch_id
 
     node_config = {
@@ -53,11 +53,32 @@ async def create_single_node(session, auth, template_name, template_id, device_n
 
         return created_node
 
+async def get_all_nodes_ids(session, auth):
+    async with session.get(f"{GNS3_URL}/projects/{PROJECT_ID}/nodes", auth=auth) as response:
+        response.raise_for_status()
+        all_nodes = await response.json()
+        return [node["node_id"] for node in all_nodes]
+
+async def delete_node(session, auth, node_id: str):
+    async with session.delete(f"{GNS3_URL}/projects/{PROJECT_ID}/nodes/{node_id}", auth=auth) as response:
+        response.raise_for_status()
 
 async def main():
     auth = aiohttp.BasicAuth(USERNAME, PASSWORD)
 
     async with aiohttp.ClientSession() as session:
+        print("Fetching all nodes ids...")
+        all_nodes_ids = await get_all_nodes_ids(session, auth)
+
+        delete_nodes_tasks = [delete_node(session, auth, node_id) for node_id in all_nodes_ids]
+        print("Deleting all nodes..")
+        results = await asyncio.gather(*delete_nodes_tasks, return_exceptions=True)
+
+        for node_id, result in zip(all_nodes_ids, results):
+            if isinstance(result, Exception):
+                print(f"Failed to delete node {node_id}: {result}")
+
+        print("Starting creating topology...")
         # 1. Fetch all templates asynchronously
         async with session.get(f"{GNS3_URL}/templates", auth=auth) as response:
             response.raise_for_status()
@@ -81,6 +102,7 @@ async def main():
 
         # Fire all node creation requests simultaneously
         created_nodes_results = await asyncio.gather(*creation_tasks)
+        print("Created all nodes")
 
         # Process results sequentially to extract IDs
         node_name_to_id = {}
@@ -103,13 +125,14 @@ async def main():
             async def post_link(link_conf):
                 url = f"{GNS3_URL}/projects/{PROJECT_ID}/links"
                 async with session.post(url, json=link_conf, auth=auth) as resp:
-                    link_res = await resp.json()
+                    await resp.json()
                     resp.raise_for_status()
 
             link_tasks.append(post_link(link_config))
 
         # Execute all link commands concurrently
         await asyncio.gather(*link_tasks)
+        print("Created all links")
 
         # 4. Start all project nodes
         async with session.post(f"{GNS3_URL}/projects/{PROJECT_ID}/nodes/start", auth=auth) as response:
@@ -118,9 +141,24 @@ async def main():
 
 
 if __name__ == "__main__":
-    # Ask for the sudo password once
-    subprocess.run(["sudo", "-v"], check=True)
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--refresh",
+        action=argparse.BooleanOptionalAction,
+        default=True,
+        help="Refresh the socat forwarder (default: enabled)."
+    )
+
+    args = parser.parse_args()
+    should_refresh_socat = args.refresh
+
+    if should_refresh_socat:
+        # Ask for the sudo password once
+        subprocess.run(["sudo", "-v"], check=True)
+
     asyncio.run(main())
 
-    print("Killing the previous socat process...")
-    subprocess.run(["sudo", "pkill", "-f", "TCP-LISTEN:8000,fork,reuseaddr"], check=True)
+    if should_refresh_socat:
+        print("Killing the previous socat process...")
+        subprocess.run(["sudo", "pkill", "-f", "TCP-LISTEN:8000,fork,reuseaddr"], check=True)
